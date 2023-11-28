0454c8f9fc072304ab250ec0eb6c4f9628f40fd5
Kumatetsu <nestor.carvacho@gmail.com>
2023-11-28 01:57:36 -0300

    tengoSuenio

--- talleres/urls.py
+++ talleres/urls.py
@@ -10,9 +10,35 @@ urlpatterns = [
     path('login',login,name='login'),
     path('LogueadoTalleresUsuario',LogueadoTalleresUsuario,name='LogueadoTalleresUsuario'),
     path('InscripcionTalleresUsuario',InscripcionTalleresUsuario,name='InscripcionTalleresUsuario'),
-    path('muestraDatosUsuario',muestraDatosUsuario,name='muestraDatosUsuario'),
+
     path('EliminarTallerUsuario',EliminarTallerUsuario,name='EliminarTallerUsuario'),
     path('evaluarTallerUsuario', evaluarTallerUsuario, name='evaluarTallerUsuario'),
+    
+    #Vista principal Funacionario
+    path('loagueadoTalleresAdmin', loagueadoTalleresAdmin, name='loagueadoTalleresAdmin'),
+    
+    #Crud Talleres desde la vista de los funcionarios
+    path('administrarTalleresFuncionario', administrarTalleresFuncionario, name='administrarTalleresFuncionario'),
+    path('crearTallerFuncionario',crearTallerFuncionario,name='crearTallerFuncionario'),
+    path('modificarTallerFuncionario',modificarTallerFuncionario,name='modificarTallerFuncionario'),
+    
+    #Crud Usuarios desde la vista de los funcionarios
+    path('administrarUsuariosFuncionario', administrarUsuariosFuncionario, name='administrarUsuariosFuncionario'),
+    path('crearUsuariosFuncionario',crearUsuariosFuncionario,name='crearUsuariosFuncionario'),
+    path('modificarUsuarioFuncionario',modificarUsuarioFuncionario,name='modificarUsuarioFuncionario'),
+    
+    #Crud Instructor desde la vista de los funcionarios
+    path('administrarInstructorFuncionario', administrarInstructorFuncionario, name='administrarInstructorFuncionario'),
+    path('crearInstructorFuncionario',crearInstructorFuncionario,name='crearInstructorFuncionario'),
+    path('modificarInstructorFuncionario',modificarInstructorFuncionario,name='modificarInstructorFuncionario'),
+    
+    #inscribir a taller vista desde el funcionario
+    path('inscribir',inscribir,name='inscribir'),
+    
+    
+    #mostrar Datos de Usuarios Logueado
+    path('muestraDatosFuncionario',muestraDatosFuncionario,name='muestraDatosFuncionario'),
+    path('muestraDatosUsuario',muestraDatosUsuario,name='muestraDatosUsuario'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- talleres/views.py
+++ talleres/views.py
@@ -24,3 +24,56 @@ def EliminarTallerUsuario(request):
 
 def evaluarTallerUsuario(request):
     return render(request, 'core/evaluarTallerUsuario.html')
+
+####################################################################################################################
+
+#crud talleres funcionmario
+def loagueadoTalleresAdmin(request):
+    return render(request,'core/mantenedores/tallerFuncionario/loagueadoTalleresAdmin.html')
+
+def administrarTalleresFuncionario(request):
+    return render(request,'core/mantenedores/tallerFuncionario/administrarTalleresFuncionario.html')
+
+def crearTallerFuncionario(request):
+    return render(request,'core/mantenedores/tallerFuncionario/crearTallerFuncionario.html')
+
+def modificarTallerFuncionario(request):
+    return render(request,'core/mantenedores/tallerFuncionario/modificarTallerFuncionario.html')
+
+####################################################################################################################
+
+#CRUD Usuarios desde funcionmario
+def administrarUsuariosFuncionario(request):
+    return render(request,'core/mantenedores/usuarioFuncionario/administrarUsuariosFuncionario.html')
+
+def crearUsuariosFuncionario(request):
+    return render(request,'core/mantenedores/usuarioFuncionario/crearUsuariosFuncionario.html')
+
+def modificarUsuarioFuncionario(request):
+    return render(request,'core/mantenedores/usuarioFuncionario/modificarUsuarioFuncionario.html')
+
+####################################################################################################################
+
+#CRUD Instructores desde funcionmario
+def administrarInstructorFuncionario(request):
+    return render(request,'core/mantenedores/instructorFuncionario/administrarInstructorFuncionario.html')
+
+def crearInstructorFuncionario(request):
+    return render(request,'core/mantenedores/instructorFuncionario/crearInstructorFuncionario.html')
+
+def modificarInstructorFuncionario(request):
+    return render(request,'core/mantenedores/instructorFuncionario/modificarInstructorFuncionario.html')
+
+####################################################################################################################
+
+# Inscribir a taller vista Funcionario
+def inscribir(request):
+    return render(request,'core/mantenedores/inscribirATaller/inscribir.html')
+
+####################################################################################################################
+
+def muestraDatosUsuario(request):
+    return render(request,'core/muestraDatosUsuario.html')
+
+def muestraDatosFuncionario(request):
+    return render(request,'core/muestraDatosFuncionario.html')
